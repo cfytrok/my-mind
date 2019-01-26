2e23474d33d7bf8ba93a57d7163467706ae1de96
cfytrok <agvrdirlog@gmail.com>
2019-01-26 15:37:30 +0300

    Update my-mind.js

--- my-mind.js
+++ my-mind.js
@@ -3597,7 +3597,7 @@ MM.Backend.Firebase._login = function(type) {
 MM.Backend.GDrive = Object.create(MM.Backend, {
 	id: {value: "gdrive"},
 	label: {value: "Google Drive"},
-	scope: {value: "https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.install"},
+	scope: {value: "https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/drive.install"},
 	clientId: {value: "993680866076-lof7sh52pscln41o0pubkbr60ngk6nr9.apps.googleusercontent.com"},
 	apiKey: {value: "AIzaSyBsfoUhSfjiZv5pB_jlYYpH3Pb647N2vF4 "},
 	fileId: {value: null, writable: true}
